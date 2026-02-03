e80807862a0720c3551bf03bec03b1b4aecfa856
RiyaGhosh09 <70480555+RiyaGhosh09@users.noreply.github.com>
2026-02-03 22:38:32 +0530

    Update 4.Finding User Purchases.sql

--- Medium/4.Finding User Purchases.sql
+++ Medium/4.Finding User Purchases.sql
@@ -1,11 +1,24 @@
 with cte as(
-select user_id, created_at as cur_orderdate,  
-lead(created_at, 1) over (partition by user_id order by user_id,created_at) as next_orderdate
-from amazon_transactions 
-order by user_id, created_at)
+select 
+    *
+from (
+    select 
+        user_id,
+        created_at as cur_orderdate,  
+        lead(created_at, 1) over (partition by user_id order by created_at) as next_orderdate,
+        row_number() over(partition by user_id order by created_at) as rn
+    from amazon_transactions 
+    )
+where rn = 1
+)
 
-select distinct user_id from(
-select *, (next_orderdate-cur_orderdate) as diff from cte 
-where (next_orderdate-cur_orderdate) != null or (next_orderdate-cur_orderdate)<=7)
-as s
-order by user_id;
+select 
+    distinct user_id 
+from(
+    select 
+        *, 
+        (next_orderdate-cur_orderdate) as diff 
+    from cte 
+    )
+where diff != null or (diff between 1 and 7)
+order by user_id
