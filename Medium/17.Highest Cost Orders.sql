4121204b82a5c890fc76b7b198936038e5c01184
RiyaGhosh09 <70480555+RiyaGhosh09@users.noreply.github.com>
2026-02-03 21:54:37 +0530

    Update 17.Highest Cost Orders.sql

--- Medium/17.Highest Cost Orders.sql
+++ Medium/17.Highest Cost Orders.sql
@@ -1,6 +1,27 @@
-select first_name, sum(total_order_cost) as total_order_cost, order_date
-from customers c join orders o on c.id = o.cust_id
+with base as (
+select 
+    first_name, 
+    order_date,
+    sum(total_order_cost) as total_order_cost
+from customers c 
+join orders o 
+on c.id = o.cust_id
 where order_date between '2019-02-01' and '2019-05-01'
-group by first_name, order_date
-order by 2 desc
-limit 1;
+group by 1,2
+order by 3 desc
+)
+
+select 
+    first_name, 
+    order_date,
+    total_order_cost
+from (
+    select 
+        first_name, 
+        order_date,
+        total_order_cost,
+        dense_rank() over(partition by order_date order by total_order_cost desc) as rnk
+    from base
+    group by 1,2,3
+    )
+where rnk = 1;
