b7c0d1cc99d0728e109d286fc9423663699d13ac
RiyaGhosh09 <70480555+RiyaGhosh09@users.noreply.github.com>
2026-02-12 14:48:28 +0530

    Create 38.Find Students At Median Writing.sql

---
+++ Medium/38.Find Students At Median Writing.sql
@@ -0,0 +1,10 @@
+select 
+    student_id
+from sat_scores
+where sat_writing = (
+    select 
+        percentile_cont(0.5) within group (order by sat_writing) as median
+    from sat_scores
+    )
+
+
